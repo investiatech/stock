7b0283980f0182cc6f6697c7d00386df257b476a
investiatech <investia.tech@gmail.com>
2024-07-14 11:58:10 +0200

    correct README file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# stock
+# Stock Application
+#License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+#Contact
+Created INVESTIA.TECH - feel free to contact me!
--- src/main/java/tech/investia/stock/services/HttpClientService.java
+++ src/main/java/tech/investia/stock/services/HttpClientService.java
@@ -20,12 +20,26 @@ public class HttpClientService {
     //    https://query1.finance.yahoo.com/v8/finance/chart/AAPL?symbol=AAPL&period1=0&period2=9999999999&interval=1d&includePrePost=true&events=div%7Csplit
     public static void httpClientService() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://query1.finance.yahoo.com/v8/finance/chart/AAPL?symbol=AAPL&period1=0&period2=9999999999&interval=1d&includePrePost=true&events=div%7Csplit"))
+                .uri(URI.create("https://query1.finance.yahoo.com/v8/finance/chart/AAPL?symbol=AAPL&period1=0&period2=9999999999&interval=1d&includePrePost=true&events=div%7Csplit")) //.formatted("AAPL")))
+//                .header("X-Finnhub-Token", "cpokoq1r01qj9etv9pdgcpokoq1r01qj9etv9pe0")
                 .GET()
                 .build();
         String response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString()).body();
 
         System.out.println(response);
+
+//        String json = "[" +
+//                response
+//                + "]";
+//        Gson gson = new Gson();
+//        // Create a List<Quote> type token
+//        Type quoteListType = new TypeToken<List<Quote>>() {
+//        }.getType();
+//        // Json array to List of objects
+//        List<Quote> list = gson.fromJson(json, quoteListType);
+//        for (Quote quote : list) {
+//            System.out.println(quote.getC() + " " + quote.getT());
+//        }
     }
 
 }
